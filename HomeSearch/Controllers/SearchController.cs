f83ad83e8b52c19492cf6efa0c65f883694ea12d
AndrewPPinner <andppinner@gmail.com>
2022-05-14 13:53:34 -0400

    more fixes

--- HomeSearch/Controllers/SearchController.cs
+++ HomeSearch/Controllers/SearchController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HomeSearch.Controllers {
-    [Route("data")]
+    [Route("home_search")]
     [ApiController]
     public class SearchController : ControllerBase {
 
